13e1012ba154969939b3036ea66c4b2e8f98716e
Sahan Bandara <sahan.bandara@pearson.com>
2022-08-07 11:21:26 +0530

    Firs commit

---
+++ components/HomeHeader.js
@@ -0,0 +1,98 @@
+import { View, Text, Image, TextInput } from 'react-native';
+
+import { COLORS, FONTS, SIZES, assets } from '../constants';
+
+const HomeHeader = ({ onSearch }) => {
+  return (
+    <View style={{
+      backgroundColor: COLORS.primary,
+      padding: SIZES.font
+    }}>
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}>
+        <Image 
+          source={assets.logo}
+          resizeMode='contain'
+          style={{
+            width: 90,
+            height: 25
+          }}
+        />
+
+        <View style={{
+          width: 45,
+          height: 45
+        }}>
+          <Image 
+            source={assets.person01}
+            resizeMode='contain'
+            style={{
+              width: '100%',
+              height: '100%'
+            }}
+          />
+          <Image 
+            source={assets.badge}
+            resizeMode='contain'
+            style={{
+              width: 15,
+              height: 15,
+              position: 'absolute',
+              bottom: 0,
+              right: 0
+            }}
+          />
+        </View>
+      </View>
+
+      <View style={{
+        marginVertical: SIZES.font
+      }}>
+        <Text style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.white
+        }}>Hello, Sahan 👋</Text>
+
+        <Text style={{
+          fontFamily: FONTS.bold,
+          fontSize: SIZES.large,
+          color: COLORS.white,
+          marginTop: SIZES.base / 2
+        }}>Let's find a masterpiece</Text>
+      </View>
+
+      <View style={{ marginTop: SIZES.font }}>
+        <View style={{
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          borderRadius: SIZES.extraLarge,
+          backgroundColor: COLORS.gray,
+          paddingHorizontal: SIZES.font,
+          paddingVertical: SIZES.small - 2
+        }}>
+          <Image 
+            source={assets.search}
+            resizeMode='contain'
+            style={{
+              width: 20,
+              height: 20,
+              marginRight: SIZES.base
+            }}
+          />
+          <TextInput
+            placeholder='Search NFTs'
+            style={{ flex: 1, color: COLORS.white }}
+            onChangeText={onSearch}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default HomeHeader;
